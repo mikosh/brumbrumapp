e589849b591780b76b4d598e9aa9a65befb631a5
Dimitar <d.s.zlatkov@gmail.com>
2019-01-20 17:14:22 +0200

    search added.

--- src/components/Trips/index.js
+++ src/components/Trips/index.js
@@ -5,6 +5,20 @@ import { withAuthorization, withEmailVerification } from '../Session';
 import Moment from 'react-moment';
 //import 'moment/locale/bg';
 import profile from '../../assets/profile.png';
+import LocationSearchInput from './places';
+import { getLatLng } from 'react-places-autocomplete';
+
+// calculate air distance between two points
+const calculateDistance = (lat1, lon1, lat2, lon2) => {
+  const R = 6371; // km
+  const dLat = (lat2-lat1)*Math.PI/180;
+  const dLon = (lon2-lon1)*Math.PI/180;
+  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+          Math.cos(lat1*Math.PI/180) * Math.cos(lat2*Math.PI/180) *
+          Math.sin(dLon/2) * Math.sin(dLon/2);
+  const c = 2 * Math.asin(Math.sqrt(a));
+  return R * c;
+}
 
 class TripsPage extends Component {
   constructor(props) {
@@ -12,6 +26,12 @@ class TripsPage extends Component {
     this.state = {
       loading: false,
       trips: [],
+      searchTrips: [],
+      searchActive: false,
+      searchStartLat: '',
+      searchStartLon: '',
+      searchEndLat: '',
+      searchEndLon: ''
     };
   }
 
@@ -25,8 +45,17 @@ class TripsPage extends Component {
     return false;
   }
 
+  filterByDistance  = (item) => {
+    const distStart = calculateDistance(this.state.searchStartLat, this.state.searchStartLon, item.startLat, item.startLon);
+    const distEnd = calculateDistance(this.state.searchEndLat, this.state.searchEndLon, item.endLat, item.endLon);
+    if (distStart < 25.1 && distEnd < 25.1) {
+      return true;
+    }
+    return false;
+  }
+
   onCollectionUpdate = (querySnapshot) => {
-    var trips = [];
+    let trips = [];
     querySnapshot.forEach((doc) => {
       const tripsObject = doc.data();
       tripsObject.id = doc.id;
@@ -48,48 +77,136 @@ class TripsPage extends Component {
     });
   }
 
+  onSearchSubmit = (e) => {
+    e.preventDefault();
+    let searchTrips = this.state.trips.filter(this.filterByDistance);
+
+    searchTrips.sort((a,b) => {
+      var c = new Date(a.leaveDate.seconds * 1000);
+      var d = new Date(b.leaveDate.seconds * 1000);
+      return c-d;
+    });
+
+    this.setState({
+      loading: false,
+      searchActive: true,
+      searchTrips
+    });
+  }
+  onRemoveFilter = (e) => {
+    this.setState({
+      searchActive: false,
+      searchTrips: []
+
+    });
+  }
+
+  setStartLocation = (googleLocation) => {
+    getLatLng(googleLocation)
+    .then(latLng => {
+      this.setState({
+        searchStartLat: latLng.lat,
+        searchStartLon: latLng.lng
+      })
+      console.log('Success', latLng)
+    })
+    .catch(error => {
+      console.error('Error', error)
+    });
+  }
+
+  setEndLocation = (googleLocation) => {
+    getLatLng(googleLocation)
+    .then(latLng => {
+      this.setState({
+        searchEndLat: latLng.lat,
+        searchEndLon: latLng.lng
+      })
+      console.log('Success', latLng)
+    })
+    .catch(error => {
+      console.error('Error', error)
+    });
+  }
+
   componentDidMount() {
     this.setState({ loading: true });
     this.props.firebase.trips().where("deleted", "==", false).onSnapshot(this.onCollectionUpdate);
   }
 
-
-
   render() {
-    const { loading, trips } = this.state;
+    const { loading, searchActive, trips, searchTrips } = this.state;
     return (
       <div className="container">
         <div className="page">
+          <Search
+            searchActive={searchActive}
+            setStartLocation={this.setStartLocation}
+            setEndLocation={this.setEndLocation}
+            onSearchSubmit={this.onSearchSubmit}
+            onRemoveFilter={this.onRemoveFilter} />
           {loading && <div>Loading ...</div>}
-          <TripsList trips={trips} />
+          <TripsList loading={loading} trips={(searchActive)? searchTrips : trips} />
         </div>
       </div>
     );
   }
 }
 
-const TripsList = ({ trips }) => (
-  <div className="list-group">
-    {trips.map(trip => (
-      <Link to={`/trips/${trip.id}`} key={trip.id} className="list-group-item list-group-item-action flex-column align-items-start">
-        <div className="d-flex w-100 justify-content-between">
-          <h5 className="mb-1">
-          <img src={trip.imageUrl? trip.imageUrl : profile} alt="Avatar" className="avatar"/>
-          <span> {trip.driverName}, {trip.driverAge}</span></h5>
-          <h5>{trip.price} {trip.currency}</h5>
-        </div>
-        <p className="mb-1">{trip.startCity} - {(trip.roundTrip)? trip.endCity + " - " + trip.startCity : trip.endCity}</p>
-        {(trip.roundTrip) ?
-          <small>
-            <Moment format="ddd, MMM Do, hh:mm">{trip.leaveDateFormated}</Moment>
-            <span> - </span>
-            <Moment format="ddd, MMM Do, hh:mm">{trip.returnDateFormated}</Moment>
-          </small>
-          :
-          <small><Moment format="ddd, MMM Do, hh:mm">{trip.leaveDateFormated}</Moment></small>
-        }
-      </Link>
-    ))}
+const Search = ({setStartLocation, setEndLocation,  onSearchSubmit, searchActive, onRemoveFilter}) => (
+  <form className="form-inline" onSubmit={onSearchSubmit}>
+    <div className="form-group mb-3">
+      <div className="input-group-prepend">
+        <span className="input-group-text">From:</span>
+      </div>
+      <LocationSearchInput setLocation={setStartLocation} text= "From" placeholder="Select start address" />
+    </div>
+    <div className="form-group mx-sm-3 mb-3">
+      <div className="input-group-prepend">
+        <span className="input-group-text">To:</span>
+      </div>
+      <LocationSearchInput setLocation={setEndLocation} placeholder="Select end address" />
+    </div>
+    <button type="submit" className="btn btn-primary btn-brum mb-3">Search</button>
+    {
+      (searchActive) ?
+        <button type="button" className="btn btn-warning mb-3 ml-2" onClick={onRemoveFilter}>Remove filter</button>
+      : ""
+    }
+  </form>
+);
+
+const TripsList = ({ loading, trips }) => (
+  <div>
+    { (!loading && trips && trips.length === 0)?
+      <div className="alert alert-info" role="alert">
+        <h3>Oh no :(</h3>
+        <p>No active trips found.</p>
+      </div>
+      : "" }
+    <div className="list-group">
+      {trips.map(trip => (
+        <Link to={`/trips/${trip.id}`} key={trip.id} className="list-group-item list-group-item-action flex-column align-items-start">
+          <div className="d-flex w-100 justify-content-between">
+            <h5 className="mb-1">
+            <img src={trip.imageUrl? trip.imageUrl : profile} alt="Avatar" className="avatar"/>
+            <span> {trip.driverName}, {trip.driverAge}</span></h5>
+            <h5>{trip.price} {trip.currency}</h5>
+          </div>
+          <p className="mb-1">{trip.startCity} - {(trip.roundTrip)? trip.endCity + " - " + trip.startCity : trip.endCity}</p>
+          {(trip.roundTrip) ?
+            <small>
+              <Moment format="ddd, MMM Do, hh:mm">{trip.leaveDateFormated}</Moment>
+              <span> - </span>
+              <Moment format="ddd, MMM Do, hh:mm">{trip.returnDateFormated}</Moment>
+            </small>
+            :
+            <small><Moment format="ddd, MMM Do, hh:mm">{trip.leaveDateFormated}</Moment></small>
+          }
+        </Link>
+      ))
+      }
+    </div>
   </div>
 );
 
