0a27490b9dd2da2e2778cdf1eb78352a8ee9cb93
A. Smith <mikosh6@hotmail.com>
2019-01-28 09:02:07 +0000

    terms and privacy translations

--- src/components/Privacy/index.js
+++ src/components/Privacy/index.js
@@ -1,70 +1,87 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withLocalize, Translate } from "react-localize-redux";
+import bgTranslations from "./bg.privacy.json";
+import enTranslations from "./en.privacy.json";
 
-const PrivacyPage = () => (
-  <div>
-  <header className="bg-gradient" id="privacy">
-    <div className="container mt-5 transparent">
-        <h1>Privacy Policy</h1>
-        <p className="tagline small">Last updated on: 26-11-2018</p>
+class PrivacyPage extends Component {
+
+  constructor(props) {
+    super(props);
+    this.props.addTranslationForLanguage(bgTranslations, "bg");
+    this.props.addTranslationForLanguage(enTranslations, "en");
+  }
+
+  render() {
+    return (
+    <div>
+    <header className="bg-gradient" id="privacy">
+      <div className="container mt-5 transparent">
+          <h1><Translate id="title" /></h1>
+          <p className="tagline small"><Translate id="updated"/></p>
+      </div>
+  </header>
+  <div className="section">
+  <div className="container">
+    <h3><Translate id="title1" /></h3>
+    <div><Translate id="desc1" /></div>
+    <div><Translate id="desc2" />
+      <ul>
+        <li>
+          <Translate id="desc3" />
+        </li>
+        <li>
+          <Translate id="desc4" />
+        </li>
+      </ul>
     </div>
-</header>
-<div className="section">
-<div className="container">
-  <h3>Information we collect</h3>
-  <div>We collect some information to provide better services to all of our users.</div>
-  <div>We collect information in the following ways:
-    <ul>
-      <li>
-        Information you give us.
-        In the profile settings, we will ask for personal information, like your name, age, sex, car, location and phone number.
-      </li>
-    </ul>
-  </div>
-  <hr/>
-  <h3>How we use information we collect</h3>
-  <div>We use the information we collect on our service to provide, maintain, protect and improve it.
-  </div>
-  <hr/>
-  <h3>Transparency and choice</h3>
-  <div>People have different privacy concerns. Our goal is to be clear about what information we collect, so that you can make meaningful choices about how it is used. For example, you can:
-    <ul>
-      <li>Delete your account.
-      </li>
-    </ul>
-  </div>
-  <div>You may also set your browser to block all cookies, including cookies associated with our services, or to indicate when a cookie is being set by us. However, it’s important to remember that many of our services may not function properly if your cookies are disabled. For example, we may not remember your language preferences.</div>
-  <hr/>
-  <h3>Information you share</h3>
-  <div>Our service let you share information with others. Remember that when you share information publicly, it may be indexable by search engines.</div>
-  <hr/>
-  <h3>Accessing and updating your personal information</h3>
-  <div>You can edit information you provide us through profile settings.</div>
-  <hr/>
-  <h3>Information we share</h3>
-  <div>We do not share personal information with companies, organizations and individuals outside of BrumBrum unless one of the following circumstances apply:
-    <ul>
-      <li>
-        For legal reasons. We will share personal information with companies, organizations or individuals outside of BrumBrum if we have a good-faith belief that access, use, preservation or disclosure of the information is reasonably necessary to:
-        <ul>
-          <li>meet any applicable law, regulation, legal process or enforceable governmental request.</li>
-          <li>enforce applicable Terms of Service, including investigation of potential violations.</li>
-          <li>detect, prevent, or otherwise address fraud, security or technical issues.</li>
-          <li>protect against harm to the rights, property or safety of BrumBrum, our users or the public as required or permitted by law.</li>
-        </ul>
-      </li>
-    </ul>
-  </div>
-  <div>We may share aggregated, non-personally identifiable information publicly and with our partners – like publishers, advertisers or connected sites. For example, we may share information publicly to show trends about the general use of our services.</div>
-  <div>If BrumBrum is involved in a merger, acquisition or asset sale, we will continue to ensure the confidentiality of any personal information and give affected users notice before personal information is transferred or becomes subject to a different privacy policy.</div>
-  <hr/>
-  <h3>Application</h3>
-  <div>Our Privacy Policy does not apply to services offered by other companies or individuals, including products or sites that may be linked from our service. Our Privacy Policy does not cover the information practices of other companies and organizations who advertise our services, and who may use cookies, pixel tags and other technologies to serve and offer relevant ads.</div>
-  <hr/>
-  <h3>Changes</h3>
-  <div>Our Privacy Policy may change from time to time. We will not reduce your rights under this Privacy Policy without your explicit consent. We will post any privacy policy changes on this page and, if the changes are significant, we will provide a more prominent notice (including, for certain services, email notification of privacy policy changes).</div>
-  </div>
-  </div>
-  </div>
-);
+    <hr/>
+    <h3><Translate id="title2" /></h3>
+    <div><Translate id="desc5" />
+    </div>
+    <hr/>
+    <h3><Translate id="title3" /></h3>
+    <div><Translate id="desc6" />
+      <ul>
+        <li><Translate id="desc7" />
+        </li>
+      </ul>
+    </div>
+    <div><Translate id="desc8" /></div>
+    <hr/>
+    <h3><Translate id="title4" /></h3>
+    <div><Translate id="desc9" /></div>
+    <hr/>
+    <h3><Translate id="title5" /></h3>
+    <div><Translate id="desc10" /></div>
+    <hr/>
+    <h3><Translate id="title6" /></h3>
+    <div><Translate id="desc11" />
+      <ul>
+        <li>
+          <Translate id="desc12" />
+          <ul>
+            <li><Translate id="desc13" /></li>
+            <li><Translate id="desc14" /></li>
+            <li><Translate id="desc15" /></li>
+            <li><Translate id="desc16" /></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+    <div><Translate id="desc17" /></div>
+    <div><Translate id="desc18" /></div>
+    <hr/>
+    <h3><Translate id="title7" /></h3>
+    <div><Translate id="desc19" /></div>
+    <hr/>
+    <h3><Translate id="title8" /></h3>
+    <div><Translate id="desc20" /></div>
+    </div>
+    </div>
+    </div>
+  );
+  }
 
-export default PrivacyPage;
+}
+
+export default withLocalize(PrivacyPage);
--- src/components/Terms/index.js
+++ src/components/Terms/index.js
@@ -1,63 +1,78 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withLocalize, Translate } from "react-localize-redux";
+import bgTranslations from "./bg.terms.json";
+import enTranslations from "./en.terms.json";
 
-const TermsPage = () => (
-  <div>
-  <header className="bg-gradient" id="terms">
-    <div className="container mt-5 transparent">
-        <h1>Terms and Conditions</h1>
-        <p className="tagline small">Last updated on: 26-11-2018</p>
+class TermsPage extends Component {
+
+  constructor(props) {
+    super(props);
+    this.props.addTranslationForLanguage(bgTranslations, "bg");
+    this.props.addTranslationForLanguage(enTranslations, "en");
+  }
+
+  render () {
+    return (
+    <div>
+    <header className="bg-gradient" id="terms">
+      <div className="container mt-5 transparent">
+          <h1><Translate id="title" /></h1>
+          <p className="tagline small"><Translate id="updated" /></p>
+      </div>
+    </header>
+    <div className="section section-terms">
+    <div className="container">
+    <h3><Translate id="title1" /></h3>
+    <p><Translate id="desc1" /></p>
+    <p><Translate id="desc2" /></p>
+    <hr/>
+    <h3><Translate id="title2" /></h3>
+    <p><Translate id="desc3" /></p>
+    <p><Translate id="desc4" /></p>
+    <hr/>
+    <h3><Translate id="title3" /></h3>
+    <p><Translate id="desc5" /></p>
+    <p><Translate id="desc6" /></p>
+    <p><Translate id="desc7" /></p>
+    <hr/>
+    <h3><Translate id="title4" /></h3>
+    <p><Translate id="desc8" /></p>
+    <p><Translate id="desc9" /></p>
+    <hr/>
+    <h3><Translate id="title5" /></h3>
+    <p><Translate id="desc10" /></p>
+    <hr/>
+    <h3><Translate id="title6" /></h3>
+    <p><Translate id="desc11" /></p>
+    <p><Translate id="desc12" /></p>
+    <p><Translate id="desc13" /></p>
+    <p><Translate id="desc14" /></p>
+
+    <h3><Translate id="title7" /></h3>
+    <p><Translate id="desc15" /></p>
+    <p><Translate id="desc16" /></p>
+    <p><Translate id="desc17" /></p>
+    <hr/>
+    <h3><Translate id="title8" /></h3>
+    <p><Translate id="desc18" /></p>
+    <p><Translate id="desc19" /></p>
+    <p><Translate id="desc20" /></p>
+    <p><Translate id="desc21" /></p>
+    <p><Translate id="desc22" /></p>
+    <p><Translate id="desc23" /></p>
+    <p><Translate id="desc24" /></p>
+    <p><Translate id="desc25" /></p>
+    <p><Translate id="desc26" /></p>
+    <hr/>
+    <h3><Translate id="title9" /></h3>
+    <p><Translate id="desc27" /></p>
+
     </div>
-  </header>
-  <div className="section section-terms">
-  <div className="container">
-  <h3>§ 1 Application</h3>
-  <p>(1) The following general terms and conditions apply to all services and offerings (“services”) advertised and performed by BrumBrum LTD. (“BrumBrum”), even if the service is accessed from outside the Republic of Bulgaria.</p>
-  <p>(2) Provisions in the general terms and conditions of BrumBrum’s contracting parties (“users”) which conflict with or deviate from the present provisions only apply if BrumBrum explicitly agrees to them in writing.</p>
-  <hr/>
-  <h3>§ 2 Services</h3>
-  <p>(1) BrumBrum currently offers its users the service as described in § 8 “BrumBrum platform”.</p>
-  <p>(2) BrumBrum reserves the right to modify or end the service at any time.</p>
-  <hr/>
-  <h3>§ 3 Obligations of the user</h3>
-  <p>(1) The use of the service is only permitted for private purposes.</p>
-  <p>(2) Users must act in a proper and considerate manner in dealings with BrumBrum and other users.</p>
-  <p>(3) Users must not abuse the access to the services. They must refrain from illegal acts. Users must protect their access data from third parties and must not make it available to them.</p>
-  <hr/>
-  <h3>§ 4 Privacy Protection</h3>
-  <p>(1) “Privacy Policy“ explains how we treat your personal data and protect your privacy when you use our service.</p>
-  <p>(2) By using our service, you agree that BrumBrum can use such data in accordance with our privacy policy.</p>
-  <hr/>
-  <h3>§ 5 BrumBrum’s rights</h3>
-  <p>(1)In the event of infringement of the present general terms and conditions or reasonable suspicion of infringement by a user, BrumBrum is entitled to exclude the user and if necessary to delete content used by or addressed to the user without delay. BrumBrum will give the user reasonable warning before exclusion, unless the infringement is so serious that BrumBrum cannot be reasonably expected to continue the use.</p>
-  <hr/>
-  <h3>§ 6 BrumBrum’s liability</h3>
-  <p>(1) BrumBrum shall not be liable for the carrying out, progress and safety of journeys. In particular, BrumBrum shall not assume liability for damage of mediated journeys based on accidents which occur before, during or after the carrying out of the journey.</p>
-  <p>(2) BrumBrum shall not be liable for loss of life, physical injury or damage to health, or for any other loss or injury which occur before, during or after the carrying out of the journey.</p>
-  <p>(3) BrumBrum is further not liable for loss due to force majeure, civil disturbance, war or natural disaster or other events outside BrumBrum’s control (e.g. strike, lockout, disruption of traffic, domestic or foreign government action).</p>
-  <p>(4) All liability beyond what is described in (1), (2) and (3) above is excluded.</p>
+    </div>
+    </div>
+  );
+  }
 
-  <h3>§ 7 Modifying and Terminating our Service</h3>
-  <p>(1) We are constantly changing and improving our service.</p>
-  <p>(2) You can stop using this Service at any time.</p>
-  <p>(3) We may also stop providing this service to you, or add or create new limits to this service at any time.</p>
-  <hr/>
-  <h3>§ 8 BrumBrum platform</h3>
-  <p>(1) Description of service: BrumBrum provides an Internet platform at www.brumbrum.club (“portal”) and client software for use by mobile devices (cell phone, GPS) for partially automated brokering of opportunities for ride sharing. Persons offering a ride (“drivers”) and persons seeking a ride (“passengers”) enter their planned route on the portal or mobile device, BrumBrum matches the route and suggests passengers or drivers. Passengers and drivers can contact each other through the portal or mobile device.</p>
-  <p>(2) Contractual relationships: BrumBrum does not provide any rides itself and is not a party to any ride share agreement. The ride share agreement is exclusively between driver and passenger. Claims arising out of the ride share agreement are exclusively between driver and passenger. BrumBrum does not guarantee successful brokerage of ride sharing opportunities. No guarantee is given. BrumBrum is not liable for successful brokerage or completion of a ride.</p>
-  <p>(3) Users: Users must sign in to use the BrumBrum platform. BrumBrum is entitled to reject applications without giving any reason. Only natural persons are entitled to register as users. Each user is responsible for checking the identity of parties to an agreement and the correctness of other data. The possibility cannot be ruled out that a user account may have false contact and other data. BrumBrum does not normally check the identity of users.</p>
-  <p>(4) User content: The portal offers users the technical opportunity to publish their own content (routes, ratings, profile information, etc.). Content published by users on the portal is not reviewed by BrumBrum and does not represent BrumBrum ’s opinion. If there are indications of abuse or illegal content, BrumBrum will however review reported content, and may delete it. By publishing content on the portal, the user warrants that it is free from third party rights and grants BrumBrum a free, unrestricted, simple right of use in the content.</p>
-  <p>(5) Obligations of the driver: Drivers must possess a valid driving licence and third party insurance. Drivers must be fit to drive at the start of a journey. Drivers are liable for complying with relevant applicable legislation and regulations, such as the road traffic regulations or the passenger transportation regulations or corresponding legislation and regulations for travel abroad.</p>
-  <p>(6) Ratings: The portal enables users to evaluate each other after completion of a ride agreement. Ratings are not normally checked by BrumBrum. Users are obliged to give only truthful information in ratings and must comply with the law. All use of the rating system other than for this intended purpose is prohibited. Specifically, users are prohibited from making abusive or improper ratings, rating themselves or having third parties submit self-ratings, including circumstances in ratings which are not connected with performance of the underlying agreement, or using ratings for a purpose not connected with the portal.</p>
-  <p>(7) BrumBrum’s rights: BrumBrum  is entitled at all times to remove user offers and requests or user profiles from the portal. BrumBrum  reserves the right to prevent excluded users from registering, including by technical filters.</p>
-  <p>(8) Deletion of a profile: Users can delete their profile at any time through BrumBrum’s profile management.</p>
-  <p>(9) Exclusion of liability: BrumBrum is not liable for loss and injury arising out of a ride share opportunity brokered on the portal. This exclusion of liability applies to all types of loss and injury, and specifically for loss of profit or indirect loss, regardless of whether this is to the user or third parties. Specifically, BrumBrum is not liable for loss or injury arising out of the failure to broker a ride share opportunity. There is no entitlement to enter into a ride share agreement.</p>
-  <hr/>
-  <h3>§ 9 Warranties and Disclaimers</h3>
-  <p>WE EXCLUDE ALL WARRANTIES. WE DON’T MAKE ANY PROMISE ABOUT THIS SERVICE. WE DON’T MAKE ANY COMMITMENTS ABOUT THE CONTENT WITHIN THIS SERVICE, ITS RELIABILITY, AVAILABILITY, OR ABILITY TO MEET YOUR NEEDS. WE PROVIDE THIS SERVICE AS DESCRIBED IN THE TERMS AND CONDITIONS.</p>
+}
 
-  </div>
-  </div>
-  </div>
-);
-
-export default TermsPage;
+export default withLocalize(TermsPage);
